932758d31b239494b99c6a5f1e480673749dab55
João Martiniano <44736477+joaomartiniano@users.noreply.github.com>
2024-05-29 18:24:12 +0100

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Inserir identificação do(a) aluno(a) aqui
+//    Formato: Turma, Nº, Nome
+//    Exemplo: 10PSI2, nº 1, Ana
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_Modulo7_ProjetoAlunos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Resolver o teste aqui
+
+            // A pasta bin\debug\ já contém os ficheiros necessários
+
+            // Identificar **SEMPRE** a resolução das questões
+            //    Exemplo: Grupo I - Questão 1
+         
+            // Sempre que terminar uma questão, faça COMMIT
+            //    Exemplo mensagem de commit: Resolução questão 1, grupo 1
+
+            // Pode fazer commit sempre que quiser, durante a resolução do teste
+            //    Exemplo mensagem de commit: Resolução
+
+        }
+    }
+}
